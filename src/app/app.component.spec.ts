926eb5dc3889cf49fa4454a223b6b76b8340ec14
Louis Mallez <louis.mallez@epitech.eu>
2019-05-06 00:37:42 +0200

    clear app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-web-client'`, () => {
+  it(`should have as title 'Harpokrat'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-web-client');
+    expect(app.title).toEqual('Harpokrat');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-web-client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Harpokrat');
   });
 });
